a6e3d64625d9b5f863421acce6bb33a299762df8
Alejandro.DGZ <87236751+3NJDGZ@users.noreply.github.com>
2023-05-31 23:42:08 +0100

    Added and changed few MySQL Files

--- SQL COMMANDS/CREATE_DB_WITH_MOCK_DATA.sql
+++ SQL COMMANDS/CREATE_DB_WITH_MOCK_DATA.sql
@@ -1,8 +1,12 @@
+-- KEY = "encryptionkey1234"
+-- SALTS randomly generated within the Python Code and then hashed via MD5
+
 -- Creating Tables:
 CREATE TABLE Player (
     PlayerID int NOT NULL AUTO_INCREMENT,
     Username varchar(50),
-    Password varchar(50),
+    Password varbinary(100),
+    Salt varchar(100),
     PRIMARY KEY (PlayerID)
 );
 CREATE TABLE CognitiveArea (
@@ -35,19 +39,14 @@ CREATE TABLE Weights (
 );
 
 -- Inserting Records into the DB:
-INSERT INTO Player (Username, Password) 
+INSERT INTO Player (Username, Password, Salt) 
 VALUES 
-	('Alejandro_DGZ', 'Password1'),
-    ('Meso', 'Password2'),
-    ('Pluto', 'Password3'),
-    ('Russo', 'Password4');
+	('AlejandroDGZ', aes_encrypt(concat('Password1', md5('salt1')), 'encryptionkey1234'), md5('salt1')),
+    ('Meso', aes_encrypt(concat('Password2', md5('salt2')), 'encryptionkey1234'), md5('salt2'));
 
 INSERT INTO CPS (PlayerID, DateCalculated, CPS)
 VALUES (1, '2023-04-15', 58.32), -- Player 1 with PlayerID 1
-	   (2, '2023-04-15', 66.74), -- Player 2 with PlayerID 2
-	   (3, '2023-04-15', 40.40), -- Player 3 with PlayerID 3
-	   (4, '2023-04-15', 86.60) -- Player 4 with PlayerID 4
-;
+	   (2, '2023-04-15', 66.74); -- Player 2 with PlayerID 2
 
 INSERT INTO CognitiveArea (Name)
 VALUES ('Memory');
@@ -60,15 +59,9 @@ VALUES ('ProblemSolving');
 
 INSERT INTO Weights (PlayerID, CognitiveAreaID, WeightValue)
 VALUES (1, 1, 0.36), (1, 2, 0.20), (1, 3, 0.30), (1, 4, 0.14), -- Player 1 with PlayerID 1
-	   (2, 1, 0.02), (2, 2, 0.18), (2, 3, 0.38), (2, 4, 0.42), -- Player 2 with PlayerID 2
-	   (3, 1, 0.38), (3, 2, 0.33), (3, 3, 0.21), (3, 4, 0.08), -- Player 3 with PlayerID 3
-	   (4, 1, 0.10), (4, 2, 0.11), (4, 3, 0.50), (4, 4, 0.29) -- Player 4 with PlayerID 4
-;
+	   (2, 1, 0.02), (2, 2, 0.18), (2, 3, 0.38), (2, 4, 0.42); -- Player 2 with PlayerID 2
 
 INSERT INTO Performance (PlayerID, CognitiveAreaID, Score)
 VALUES (1, 1, 30), (1, 2, 74), (1, 3, 96), (1, 4, 28), -- Player 1 with PlayerID 1
-	   (2, 1, 54), (2, 2, 69), (2, 3, 97), (2, 4, 39), -- Player 2 with PlayerID 2
-	   (3, 1, 42), (3, 2, 20), (3, 3, 64), (3, 4, 55), -- Player 3 with PlayerID 3 
-	   (4, 1, 93), (4, 2, 75), (4, 3, 83), (4, 4, 95) -- Player 4 with PlayerID 4
-;
+	   (2, 1, 54), (2, 2, 69), (2, 3, 97), (2, 4, 39); -- Player 2 with PlayerID 2
 
--- SQL COMMANDS/search_all_items_db.sql
+++ SQL COMMANDS/search_all_items_db.sql
@@ -1,5 +1,7 @@
-select * 
+select PlayerID, Username, replace(cast(aes_decrypt(Password, 'encryptionkey1234') as char(100)), Salt, ''), Salt
 from Player;
+-- select * 
+-- from Player;
 select * 
 from CPS;
 select * 
