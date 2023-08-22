18cb4c041986478a394a4e7ea6b13e05eed3aa03
Alejandro.DGZ <87236751+3NJDGZ@users.noreply.github.com>
2023-08-22 15:23:23 +0100

    cleaning code

--- SQL COMMANDS/CREATE_DB.sql
+++ SQL COMMANDS/CREATE_DB.sql
@@ -37,16 +37,6 @@ CREATE TABLE Weights (
     FOREIGN KEY (CognitiveAreaID) REFERENCES CognitiveArea(CognitiveAreaID)
 );
 
--- Inserting Records into the DB:
--- INSERT INTO Player (Username, Password, Salt) 
--- VALUES 
--- 	('AlejandroDGZ', aes_encrypt(concat('Password1', md5('salt1')), 'encryptionkey1234'), md5('salt1')),
---     ('Meso', aes_encrypt(concat('Password2', md5('salt2')), 'encryptionkey1234'), md5('salt2'));
-
--- INSERT INTO CPS (PlayerID, DateCalculated, CPS)
--- VALUES (1, '2023-04-15', 58.32), -- Player 1 with PlayerID 1
--- 	   (2, '2023-04-15', 66.74); -- Player 2 with PlayerID 2
-
 INSERT INTO CognitiveArea (Name)
 VALUES ('Memory');
 INSERT INTO CognitiveArea (Name)
@@ -55,12 +45,3 @@ INSERT INTO CognitiveArea (Name)
 VALUES ('Speed');
 INSERT INTO CognitiveArea (Name)
 VALUES ('ProblemSolving');
-
--- INSERT INTO Weights (PlayerID, CognitiveAreaID, WeightValue)
--- VALUES (1, 1, 0.36), (1, 2, 0.20), (1, 3, 0.30), (1, 4, 0.14), -- Player 1 with PlayerID 1
--- 	   (2, 1, 0.02), (2, 2, 0.18), (2, 3, 0.38), (2, 4, 0.42); -- Player 2 with PlayerID 2
-
--- INSERT INTO Performance (PlayerID, CognitiveAreaID, Score)
--- VALUES (1, 1, 30), (1, 2, 74), (1, 3, 96), (1, 4, 28), -- Player 1 with PlayerID 1
--- 	   (2, 1, 54), (2, 2, 69), (2, 3, 97), (2, 4, 39); -- Player 2 with PlayerID 2
-
